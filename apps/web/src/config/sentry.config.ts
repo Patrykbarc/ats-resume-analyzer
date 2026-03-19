49fb2c93963d1255652927a6563344d7a9b18811
Patryk Barć <108985559+Patrykbarc@users.noreply.github.com>
2026-03-19 21:06:14 +0100

    feat(sentry): enhance Sentry configuration with tracing integration and sample rate

--- apps/web/src/config/sentry.config.ts
+++ apps/web/src/config/sentry.config.ts
@@ -4,5 +4,7 @@ import * as Sentry from '@sentry/react'
 Sentry.init({
   dsn: getEnvs().VITE_SENTRY_DSN,
   sendDefaultPii: true,
-  enabled: getEnvs().VITE_NODE_ENV === 'production'
+  enabled: getEnvs().VITE_NODE_ENV === 'production',
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0
 })
