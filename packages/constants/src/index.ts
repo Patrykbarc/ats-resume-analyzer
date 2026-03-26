1fb0fe2a859a7ebb1978111cd8ee3d189ee1faec
Patryk Barć <108985559+Patrykbarc@users.noreply.github.com>
2026-03-26 22:32:19 +0100

    feat(limiter): improve error handling in Redis command and update rate limit headers

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,9 +1,9 @@
 const FREE_REQUESTS_PER_DAY = 2
 
 const HEADERS = {
-  'X-RateLimit-Limit': 'X-RateLimit-Limit',
-  'X-RateLimit-Remaining': 'X-RateLimit-Remaining',
-  'X-RateLimit-Reset': 'X-RateLimit-Reset'
+  'X-RateLimit-Limit': 'x-ratelimit-limit',
+  'X-RateLimit-Remaining': 'x-ratelimit-remaining',
+  'X-RateLimit-Reset': 'x-ratelimit-reset'
 } as const
 
 export { FREE_REQUESTS_PER_DAY, HEADERS }
